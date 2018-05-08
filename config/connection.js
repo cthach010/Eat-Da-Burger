d31333015de6317808b0192e41e9e3378054c76f
christina thach <christinathach@christinas-MacBook-Pro.local>
2018-05-08 14:54:55 -0400

    committing

---
+++ config/connection.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection 
+  (process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password:'',
+database:'burgers'
+  });
+}
+
+
+var connection = mysql.createConnection({
+  port: 8889,
+  host: "127.0.0.1",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+//connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Bangin Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Shiitake Cheeseburgers', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Crispy Chicken Cheddar', false, CURRENT_TIMESTAMP);
